694b020354c78fe7348829f97e773791d947640c
joaoliveirapb <joaoliveira.batista1@gmail.com>
2021-10-27 21:26:41 -0300

    layout ok

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(window).scroll(function() {
+    var windowTop = $(this).scrollTop()
+    $('.anime').each(function() {
+        if (windowTop > $(this).offset().top - 500) {
+            $(this).addClass('anime-init')
+        } else {
+            $(this).removeClass('anime-init')
+        }
+    })
+})
